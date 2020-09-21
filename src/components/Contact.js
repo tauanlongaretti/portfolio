4ef35a62b334720a016a6b63f38081f3575ca303
tauan-longaretti <tauan-longaretti.com>
2020-09-20 23:25:49 -0400

    ribbon concept implemented for contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,12 @@ const Contact = () => {
     e.preventDefault();
 
     emailjs
-      .sendForm("gmail", "template_cr2nbca", e.target, "user_l5fNQ8O2z7MfawGOFLH3M")
+      .sendForm(
+        "gmail",
+        "template_cr2nbca",
+        e.target,
+        "user_l5fNQ8O2z7MfawGOFLH3M"
+      )
       .then(
         (result) => {
           console.log(result.text);
@@ -15,21 +20,30 @@ const Contact = () => {
           console.log(error.text);
         }
       );
-      e.target.reset()
+    e.target.reset();
   }
 
   return (
-    <form className="contact-form" onSubmit={sendEmail}>
-      <label>Subject</label>
-      <input type="text" name="subject" />
-      <label>Name</label>
-      <input type="text" name="name" />
-      <label>Email</label>
-      <input type="email" name="email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className="form-wrapper">
+      <div className="form-container p-4">
+        <div className="form-ribbon">
+          <form
+            className="contact-form d-flex flex-column"
+            onSubmit={sendEmail}
+          >
+            <label className="text-black mb-1">Subject</label>
+            <input type="text" name="subject" />
+            <label className="text-black mt-2 mb-1">Name</label>
+            <input type="text" name="name" />
+            <label className="text-black mt-2 mb-1">Email</label>
+            <input type="email" name="email" />
+            <label className="text-black mt-2 mb-1">Message</label>
+            <textarea name="message" />
+            <input className="mt-4" type="submit" value="Send" />
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1 className="white-text">Hi, I am <span className="gold-text">Tauan Longaretti</span></h1>
+      <h1 className="light-text">Hi, I am <span className="gold-text">Tauan Longaretti</span></h1>
     </div>
   );
 };
