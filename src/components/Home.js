7c3fdc03c86baa567a37f20964e7ddc2e39e3e8f
tauan-longaretti <tauan-longaretti.com>
2020-09-25 17:32:41 -0400

    right section of home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,18 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1 className="light-text">
-        Hi, I am <span className="gold-text">Tauan Longaretti</span>
-      </h1>
+      <div className="home-right-section">
+        <div>
+          <h1 className="light-text home-header">Hello, I am</h1>
+          <h1 className="gold-text">Tauan Longaretti</h1>
+          <p className="light-text home-text">A full stack web developer</p>
+        </div>
+        <div className="home-buttons d-flex justify-content-between">
+          <button className="white-button">About</button>
+          <button className="gold-button">Projects</button>
+          <button className="white-button">Contact</button>
+        </div>
+      </div>
     </div>
   );
 };
