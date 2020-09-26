55a03fbc2500c13365bc4e8d0c4de8ec7e13a410
tauan-longaretti <tauan-longaretti.com>
2020-09-25 20:07:51 -0400

    styled home component with profile picture implemented

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ProfilePic from "../assets/ProfilePic.png";
 
 const Home = () => {
   return (
-    <div>     
-      <div className="home-right-section">
+      <div className="d-flex home-wrapper">
         <div>
-          <h1 className="light-text home-header">Hello, I am</h1>
-          <h1 className="gold-text">Tauan Longaretti</h1>
-          <p className="light-text home-text">A full stack web developer</p>
+          <img src={ProfilePic} className="profile-picture" />
         </div>
-        <div className="home-buttons d-flex justify-content-between">
-          <Link to="/about">
-            <button className="white-button">About</button>
-          </Link>
-          <Link to="/projects">
-            <button className="gold-button">Projects</button>
-          </Link>
-          <Link to="/contact">
-            <button className="white-button">Contact</button>
-          </Link>
+        <div className="home-right-section">
+          <div>
+            <h1 className="light-text home-header">Hello, I am</h1>
+            <h1 className="gold-text">Tauan Longaretti</h1>
+            <p className="light-text home-text">A full stack web developer</p>
+          </div>
+          <div className="home-buttons d-flex justify-content-between">
+            <Link to="/about">
+              <button className="white-button">About</button>
+            </Link>
+            <Link to="/projects">
+              <button className="gold-button">Projects</button>
+            </Link>
+            <Link to="/contact">
+              <button className="white-button">Contact</button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
