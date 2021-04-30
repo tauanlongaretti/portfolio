f01a3f680c5fa001e770bd53f0ebded8455ca6dc
tauan-longaretti <tauanlongaretti@gmail.com>
2021-04-30 00:39:03 -0400

    First Project component

--- src/components/FirstProject.js
+++ src/components/FirstProject.js
@@ -1,13 +1,19 @@
 import React from "react";
 import MovieBrowsing from "../assets/MovieBrowsing.jpg";
+import {FiArrowRightCircle, FiArrowLeftCircle} from "react-icons/fi";
 
 const FirstProject = () => {
   return (
     <div className="project-wrapper">
-      <h2 className="project-title">Movie Browsing</h2>
-      <div className="project-ribbon">
-        <img className="project-image" src={MovieBrowsing} />
+      <FiArrowLeftCircle className="switch-icon"/>
+      <div className="project-container">
+        <h2 className="project-title">Movie Browsing</h2>
+        <div className="project-ribbon">
+          <img className="project-image" src={MovieBrowsing} />
+        </div>
+        <h2 className="project-description">A movie library app that provides the user many ways of browsing for a movie. React, Javascript and The Movie Database API were used in the development of this application. "Click Here" or on the image above to view this project. </h2>
       </div>
+      <FiArrowRightCircle className="switch-icon"/>
     </div>
   );
 };
