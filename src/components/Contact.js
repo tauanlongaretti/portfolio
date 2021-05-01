1705da9bbb046ddd42f49b65f7d5ed74d22f45b4
tauan-longaretti <tauanlongaretti@gmail.com>
2021-05-01 08:37:01 -0400

    fixed bug for about and contact component that would not allow it to be responssive upon nav-menu opening

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,38 +26,33 @@ const Contact = () => {
   }
 
   return (
-    <div className="form-wrapper">
-      <div className="form-container p-4">
-        <div className="form-text">
-          <h1 className="mt-2 pb-2">Contact Me</h1>
-          <p className="mt-4">Thank you for taking the </p>
-          <p className="pb-3">time to reach out.</p>
-          <p className="mt-4">Also feel free to contact </p>
-          <p>me on any of my social </p>
-          <p>media accounts:</p>
-          <a href="https://www.linkedin.com/in/tauan-longaretti-57879b194/">
-            <LinkedInIcon className="icon" color="action" />
-          </a>
-          <a href="https://twitter.com/TauanL10">
-            <TwitterIcon className="icon" href="" color="disabled" />
-          </a>
-        </div>
-        <div className="form-ribbon">
-          <form
-            className="contact-form d-flex flex-column"
-            onSubmit={sendEmail}
-          >
-            <label className="text-black mb-1">Subject</label>
-            <input type="text" name="subject" />
-            <label className="text-black mt-2 mb-1">Name</label>
-            <input type="text" name="name" />
-            <label className="text-black mt-2 mb-1">Email</label>
-            <input type="email" name="email" />
-            <label className="text-black mt-2 mb-1">Message</label>
-            <textarea name="message" className="message-input" />
-            <input className="mt-4 send-button" type="submit" value="Send" />
-          </form>
-        </div>
+    <div className="form-container p-4">
+      <div className="form-text">
+        <h1 className="mt-2 pb-2">Contact Me</h1>
+        <p className="mt-4">Thank you for taking the </p>
+        <p className="pb-3">time to reach out.</p>
+        <p className="mt-4">Also feel free to contact </p>
+        <p>me on any of my social </p>
+        <p>media accounts:</p>
+        <a href="https://www.linkedin.com/in/tauan-longaretti-57879b194/">
+          <LinkedInIcon className="icon" color="action" />
+        </a>
+        <a href="https://twitter.com/TauanL10">
+          <TwitterIcon className="icon" href="" color="disabled" />
+        </a>
+      </div>
+      <div className="form-ribbon">
+        <form className="contact-form d-flex flex-column" onSubmit={sendEmail}>
+          <label className="text-black mb-1">Subject</label>
+          <input type="text" name="subject" />
+          <label className="text-black mt-2 mb-1">Name</label>
+          <input type="text" name="name" />
+          <label className="text-black mt-2 mb-1">Email</label>
+          <input type="email" name="email" />
+          <label className="text-black mt-2 mb-1">Message</label>
+          <textarea name="message" className="message-input" />
+          <input className="mt-4 send-button" type="submit" value="Send" />
+        </form>
       </div>
     </div>
   );
