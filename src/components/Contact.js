b53ad7db5ee25137226e9404db2c238790783155
tauan-longaretti <tauanlongaretti@gmail.com>
2021-05-03 07:16:17 -0400

    contact component fully responsive

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,24 +26,11 @@ const Contact = () => {
   }
 
   return (
-    <div className="form-container p-4">
-      <div className="form-text">
-        <h1 className="mt-2 pb-2">Contact Me</h1>
-        <p className="mt-4">Thank you for taking the </p>
-        <p className="pb-3">time to reach out.</p>
-        <p className="mt-4">Also feel free to contact </p>
-        <p>me on any of my social </p>
-        <p>media accounts:</p>
-        <a href="https://www.linkedin.com/in/tauan-longaretti-57879b194/">
-          <LinkedInIcon className="icon" color="action" />
-        </a>
-        <a href="https://twitter.com/TauanL10">
-          <TwitterIcon className="icon" href="" color="disabled" />
-        </a>
-      </div>
+    <div className="form-container">
+      <h1 className="contact-header-mob gold-text">Contact Me</h1>
       <div className="form-ribbon">
         <form className="contact-form d-flex flex-column" onSubmit={sendEmail}>
-          <label className="text-black mb-1">Subject</label>
+          <label className="text-black my-1 pt-2">Subject</label>
           <input type="text" name="subject" />
           <label className="text-black mt-2 mb-1">Name</label>
           <input type="text" name="name" />
@@ -53,6 +40,22 @@ const Contact = () => {
           <textarea name="message" className="message-input" />
           <input className="mt-4 send-button" type="submit" value="Send" />
         </form>
+      </div>
+      <div className="form-text">
+        <h1 className="contact-header">Contact Me</h1>
+        <p className="">Thank you for taking the </p>
+        <p className="">time to reach out.</p>
+        <p className="mt-4">Also feel free to contact </p>
+        <p>me on any of my social </p>
+        <p>media accounts:</p>
+        <div className="social-buttons">
+          <a href="https://www.linkedin.com/in/tauan-longaretti-57879b194/">
+            <LinkedInIcon className="icon" color="action" />
+          </a>
+          <a href="https://twitter.com/TauanL10">
+            <TwitterIcon className="icon" href="" color="disabled" />
+          </a>
+        </div>
       </div>
     </div>
   );
